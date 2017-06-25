1e0bccec49d519dbb3a5cbf88dfcfa71b3ac2336
Alyssa Nielsen <annhx3@users.noreply.github.com>
2017-06-25 11:43:48 -0500

    Add files via upload

---
+++ CalculatorWPF/CalculatorWPF/Display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlyssaNielsenWPFCalculator
+{
+    class Display : System.Windows.Controls.TextBox
+    {
+        protected override void OnPreviewGotKeyboardFocus(System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            e.Handled = true;
+            base.OnPreviewGotKeyboardFocus(e);
+        }
+
+    }
+}
